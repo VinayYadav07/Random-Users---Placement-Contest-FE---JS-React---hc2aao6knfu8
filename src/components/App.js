8794e04c166e3a753cb7a143206f5c222bcab634
VinayYadav07 <98084463+VinayYadav07@users.noreply.github.com>
2023-09-03 18:23:26 +0530

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,41 @@ import axios from "axios";
 import Users from "./users";
 
 const App = () => {
-    const [items, setItems] = useState([]);
-    const [isLoading, setLoading] = useState(true);
+    const [loading, setLoading] = useState([]);
+    const [userData, setUserData] = useState([]);
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        fetch('https://randomuser.me/api/?results=10')
+        .then((Response) => Response.json())
+        .then((data) =>{
+            setTimeout(() =>{
+                setUserData(data.results);
+                setLoading(false);
+            }, 3000);
+        })
+    }, []);
 
-    return <Users isLoading={isLoading} items={items} />;
+    return (
+        <div className="App">
+            {loading ?(
+                <div id="loading">Loading....</div>
+            ): (
+                <User userData={userData} />
+            )}
+            </div>
+    )
 };
+
+function User({ userData }) {
+    return(
+        <div>
+            {userData.map((user, index) => (
+                <div key={index} className="user">
+                    {user.name.first}
+                </div>
+            ))}
+        </div>
+    );
+}
 
 export default App;
